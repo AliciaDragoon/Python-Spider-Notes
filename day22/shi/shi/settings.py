d00b9339789a1802f5db271cde666e126fd39a76
AliciaDragoongit config --global user.email edogawaai@hotmail.comgit config --global user.name AliciaDragoon <edogawaai@hotmail.com>
2024-04-09 18:17:57 +0800

    2024/4/9

--- day22/shi/shi/settings.py
+++ day22/shi/shi/settings.py
@@ -11,13 +11,14 @@ BOT_NAME = "shi"
 
 SPIDER_MODULES = ["shi.spiders"]
 NEWSPIDER_MODULE = "shi.spiders"
+LOG_LEVEL = "WARNING"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "shi (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- day22/shi/shi/spiders/tang.py
+++ day22/shi/shi/spiders/tang.py
@@ -9,14 +9,18 @@ class TangSpider(CrawlSpider):
     start_urls = ["https://www.shicimingjv.com/tangshi/index_1.html"]
 
     # 链接提取器提取详情页的url
-    lk1 = LinkExtractor(restrict_xpaths="//div[@class='article-list']/ul/li/div/h3/a")
+    lk1 = LinkExtractor(restrict_xpaths="//div[@class='sec-panel-body']/ul/li/div/h3/a")
+    # 提取页码的url
+    lk2 = LinkExtractor(restrict_xpaths="//ul[@class='pagination']/li/a")
     rules = (
-        Rule(lk1, callback="parse_item", follow=True),
+        # crawlspider会自动发送请求，并提取链接（完成了parse的工作）
+        Rule(lk1, callback="parse_item"),
+        Rule(lk2, follow=True),
+        # follow=True表示重新来一遍
+        # Rule(lk1, callback="parse_item", follow=True),
     )
 
-    def parse_item(self, response):
-        item = {}
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        #item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+    def parse_item(self, response, **kwargs):
+        title = response.xpath("//h1[@class='mp3']/text()").extract_first()
+        print(title)
+        # 共120首
